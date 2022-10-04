e950f2a1ee224f58e3df67d81105778f3757ccb4
atjenkin <atjenkin@andrew.cmu.edu>
2022-10-04 11:45:58 -0700

    Wire Prefab and scripts are connected to prefabs

--- Assets/Scripts/Battery.cs
+++ Assets/Scripts/Battery.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Battery : MonoBehaviour
 {
+    
+    private Rigidbody rigidbodyComponent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbodyComponent = GetComponent<Rigidbody>(); // shorthand for rigidbody component
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Berkeley Spice
     }
 }
--- Assets/Scripts/Wire.cs
+++ Assets/Scripts/Wire.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class View : MonoBehaviour
+public class Wire : MonoBehaviour
 {
+    
+    private Rigidbody rigidbodyComponent;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rigidbodyComponent = GetComponent<Rigidbody>(); // shorthand for rigidbody component
     }
 
     // Update is called once per frame
     void Update()
     {
-        // where to implement touch controls
+        // Berkeley Spice
     }
 }
