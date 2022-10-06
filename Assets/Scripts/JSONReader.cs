3cb6fec274fe48d887c4614a4f9bf9e5c997bb9f
atjenkin <atjenkin@andrew.cmu.edu>
2022-10-06 11:46:36 -0700

    filled JSON file with sample data and implemented JSON serialization

--- Assets/Scripts/JSONReader.cs
+++ Assets/Scripts/JSONReader.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class JSONReader : MonoBehaviour
 {
+
+    public TextAsset textJSON;
+    
+    [System.Serializable]
+
+    public class Lab {
+        public string labTitle;
+    }
+
+    [System.Serializable]
+
+    public class LabList {
+        public Lab[] Labs;
+    }
+
+    public LabList labs = new LabList();
     // Start is called before the first frame update
     void Start()
     {
-        
+        labs = JsonUtility.FromJson<LabList>(textJSON.text);
     }
 
     // Update is called once per frame
