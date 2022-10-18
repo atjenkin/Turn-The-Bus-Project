052fa8623933318d5220d0d207f57784f6bc32e2
ziniuwan <wanzn96@gmail.com>
2022-10-17 22:01:58 -0700

    the lab is parsed from json file now

---
+++ Assets/Scripts/circuitComponent/VoltageSource.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoltageSource : CircuitComponent
+{
+    public override void InitSpiceEntity(string name, string[] interfaces, float[] parameters)
+    {
+        spiceEntity = new SpiceSharp.Components.VoltageSource(name, interfaces[0], interfaces[1], parameters[0]);
+    }
+}
