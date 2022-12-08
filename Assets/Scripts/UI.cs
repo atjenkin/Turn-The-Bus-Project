c7bc6f8cf7372e1d5074ab32fc4665e54907bf8a
Harshit Maheshwari <harshitmah98@gmail.com>
2022-12-07 23:55:45 -0800

    Added contribution comments

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,3 +1,8 @@
+/*
+* This file was developed by a team from Carnegie Mellon University as a part of the practicum project for Fall 2022 in collaboration with Turn The Bus.
+* Authors: Adrian Jenkins, Harshit Maheshwari, and Ziniu Wan. (Carnegie Mellon University)
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
